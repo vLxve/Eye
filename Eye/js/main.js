f32a3edc8225eb735b994d31a5808fe74539b9c3
vLxve <107662761+vLxve@users.noreply.github.com>
2023-12-26 21:35:07 -0600

    Removed Comments as requested

--- Eye/js/main.js
+++ Eye/js/main.js
@@ -5,7 +5,6 @@ var HexgonStyleObject = {
     webkitTextStroke: "1.5px #7fc8bb"
 };
 
-// Function to apply hexagon styles
 function applyHexagonStyles() {
     $('#hex').css({
         'text-shadow': HexgonStyleObject.textShadow,
@@ -27,25 +26,20 @@ Eye.addNuiListener('ToggleEye', (Data) => {
     InEye = Data.State;
     if (InEye) {
         $('.eye-wrapper').show().css('pointer-events', 'auto');
-        applyHexagonStyles(); // Apply styles when eye is active
+        applyHexagonStyles();
     } else {
         $('.eye-wrapper').hide().css('pointer-events', 'none');
         $('.eye-options').removeClass('show-options');
-        // Reset hexagon styles when eye is inactive
         $('#hex').css({
             'text-shadow': 'none',
             '-webkit-text-stroke': 'none'
         });
 
-        // Remove the active-text-shadow class when the eye is closed
         $('#hex').removeClass('active-text-shadow');
 
-        // Revert eye color to default gray when the eye is closed
         $('.eye').css('color', 'gray');
     }
 });
-
-// Rest of your code...
 
 
 Eye.addNuiListener('SetOptions', (Data) => {
@@ -56,43 +50,33 @@ Eye.addNuiListener('SetOptions', (Data) => {
     $eyeOptions.empty();
 
     if (Data.Options.length >= 1) {
-        // Update Eye icon class if needed
         $eye.removeClass('fas fa-eye').addClass('fas fa-eye');
 
-        // Update eye color to #1ccccc
         $eye.css('color', '#1ccccc');
 
-        // Generating options HTML with background styles
         let Options = '';
         for (let i = 0; i < Data.Options.length; i++) {
             const Elem = Data.Options[i];
-            // Add background styles within the generated HTML
             Options += `<div data-parent="${Elem.Parent}" data-name="${Elem.Name}" class="option archivo" style="color: ${this.StandardColor}; background-color: rgba(180, 0, 255, 0.3); border-radius: 8px; padding: 5px 5px; margin-bottom: 5px; box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);">
                             <i class="${Elem.Icon || 'fas fa-eye'}"></i> ${Elem.Label}
                         </div>`;
         }
 
-        // Update Hexagon styles after changing Eye icon class
-        HexgonStyleObject.textShadow = "#1ccccc 0px 0 20px"; // Update text shadow
-        HexgonStyleObject.webkitTextStroke = "1.5px #1ccccc"; // Update text stroke
+        HexgonStyleObject.textShadow = "#1ccccc 0px 0 20px";
+        HexgonStyleObject.webkitTextStroke = "1.5px #1ccccc";
         updateHexagonStyles();
 
-        // Add a class to #hex to enable the text-shadow
         $hex.addClass('active-text-shadow');
 
-        // Add options HTML with fade-in class and trigger the transition
         $eyeOptions.html(Options).hide().fadeIn(250);
     } else {
-        // If there are no options, revert Eye icon class and hexagon styles
         $eye.removeClass('fas fa-eye').addClass('fas fa-eye');
-        HexgonStyleObject.textShadow = "none"; // Set text shadow to none
-        HexgonStyleObject.webkitTextStroke = "none"; // Set text stroke to none
+        HexgonStyleObject.textShadow = "none"; 
+        HexgonStyleObject.webkitTextStroke = "none";
         updateHexagonStyles();
 
-        // Revert eye color to default
         $eye.css('color', 'gray');
 
-        // Remove the class to #hex to disable the text-shadow
         $hex.removeClass('active-text-shadow');
     }
 });
