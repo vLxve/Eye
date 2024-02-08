39b43222fedff40c4e2f7cd7e60bc0887db7249f
vLxve <107662761+vLxve@users.noreply.github.com>
2024-02-08 03:00:28 -0600

    Update main.js
    New Changes

--- Eye/js/main.js
+++ Eye/js/main.js
@@ -1,8 +1,8 @@
 var Eye = RegisterApp('Eye');
 var InEye = false;
 var HexgonStyleObject = {
-    textShadow: "#00ffe1 0px 0 20px",
-    webkitTextStroke: "1.5px #7fc8bb"
+    textShadow: "#08f7c5 0px 0 20px",
+    webkitTextStroke: "1.5px #08f7c5"
 };
 
 function applyHexagonStyles() {
@@ -37,10 +37,9 @@ Eye.addNuiListener('ToggleEye', (Data) => {
 
         $('#hex').removeClass('active-text-shadow');
 
-        $('.eye').css('color', 'gray');
+        $('.eye').css('color', '#3c575767');
     }
 });
-
 
 Eye.addNuiListener('SetOptions', (Data) => {
     const $eye = $('.eye');
@@ -52,30 +51,29 @@ Eye.addNuiListener('SetOptions', (Data) => {
     if (Data.Options.length >= 1) {
         $eye.removeClass('fas fa-eye').addClass('fas fa-eye');
 
-        $eye.css('color', '#1ccccc');
+        $eye.css('color', '#08f7c5');
 
         let Options = '';
         for (let i = 0; i < Data.Options.length; i++) {
             const Elem = Data.Options[i];
-            Options += `<div data-parent="${Elem.Parent}" data-name="${Elem.Name}" class="option archivo" style="color: ${this.StandardColor}; background-color: rgba(180, 0, 255, 0.3); border-radius: 8px; padding: 5px 5px; margin-bottom: 5px; box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);">
+            Options += `<div data-parent="${Elem.Parent}" data-name="${Elem.Name}" class="option archivo" style="color: ${this.StandardColor}; background-color: rgba(8, 247, 197, 0.3); border-radius: 8px; padding: 5px 5px; margin-bottom: 5px; box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.3);">
                             <i class="${Elem.Icon || 'fas fa-eye'}"></i> ${Elem.Label}
                         </div>`;
         }
 
-        HexgonStyleObject.textShadow = "#1ccccc 0px 0 20px";
-        HexgonStyleObject.webkitTextStroke = "1.5px #1ccccc";
+        HexgonStyleObject.textShadow = "#3c575767 0px 0 20px";
+        HexgonStyleObject.webkitTextStroke = "1.5px #08f7c5";
         updateHexagonStyles();
 
         $hex.addClass('active-text-shadow');
-
-        $eyeOptions.html(Options).hide().fadeIn(250);
+        $eyeOptions.html(Options)
     } else {
         $eye.removeClass('fas fa-eye').addClass('fas fa-eye');
         HexgonStyleObject.textShadow = "none"; 
         HexgonStyleObject.webkitTextStroke = "none";
         updateHexagonStyles();
 
-        $eye.css('color', 'gray');
+        $eye.css('color', '#3c575767');
 
         $hex.removeClass('active-text-shadow');
     }
@@ -84,7 +82,7 @@ Eye.addNuiListener('SetOptions', (Data) => {
 $(document).on('click', '.eye-options .option', function(e){
     e.preventDefault();
     if (InEye) {
-        $.post("https://mercy-ui/Eye/Click", JSON.stringify({
+        $.post("https://vlxve-ui/Eye/Click", JSON.stringify({
             Parent: $(this).attr("data-parent"),
             Name: $(this).attr("data-name"),
         }));
@@ -96,7 +94,7 @@ $(document).keyup(function(e){
 
     switch (e.keyCode) {
         case 27:
-            $.post("https://mercy-ui/Eye/Close");
+            $.post("https://vlxve-ui/Eye/Close");
             break;
     }
 });
@@ -105,6 +103,6 @@ window.addEventListener("mousedown", function onEvent(event) {
     if (!InEye) return;
 
     if (event.button == 2) {
-        $.post("https://mercy-ui/Eye/Unfocus");
+        $.post("https://vlxve-ui/Eye/Unfocus");
     }
 });
